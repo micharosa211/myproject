fe4982d1e8611653952c8e882319f4a92a76f3e2
micharosa211 <112971548+micharosa211@users.noreply.github.com>
2023-03-07 14:32:15 +0100

    improvements

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,4 @@
-document.getElementById('appointment-form').addEventListener('submit', function (e) {
+document.getElementById('contact-form').addEventListener('submit', function (e) {
     e.preventDefault();
 
     const appointmentMessage = document.querySelector('.appointment-message');
@@ -6,13 +6,13 @@ document.getElementById('appointment-form').addEventListener('submit', function 
     let allFields = false;
 
     let appointment = {
-        name: document.getElementById('appointment-name').value,
-        email: document.getElementById('appointment-email').value,
+        name: document.getElementById('contact-name').value,
+        email: document.getElementById('contact-email').value,
         service: document.getElementById('appointment-service').value,
-        phone: document.getElementById('appointment-phone').value,
-        date: document.getElementById('appointment-date').value,
-        time: document.getElementById('appointment-time').value,
-        message: document.getElementById('appointment-message').value,
+        phone: document.getElementById('contact-phone').value,
+        date: document.getElementById('contact-date').value,
+        time: document.getElementById('contact-time').value,
+        message: document.getElementById('contact-message').value,
 
     }
 
@@ -39,4 +39,8 @@ document.getElementById('appointment-form').addEventListener('submit', function 
     }
 
 
+})
+
+document.getElementsByClassName('nav-link')[0].addEventListener('mouseenter', function () {
+document.getElementsByClassName('dropmenu')[0].classList.toggle('open') ;
 })
